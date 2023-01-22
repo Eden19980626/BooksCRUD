011110ade22c38b9a4fa8a7e1a0cf204534b800d
Dengz <dengweiyi870626@gmail.com>
2023-01-22 13:27:27 +0800

    adding API to db.json

--- src/App.js
+++ src/App.js
@@ -14,23 +14,6 @@ function App() {
     fetchBooks();
   }, []);
 
-  const editBookById = (id, newTitle) => {
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, title: newTitle };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = (id) => {
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(updatedBooks);
-  };
-
   const handleCreateBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', {
       title,
@@ -39,6 +22,29 @@ function App() {
     const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   return (
     <div>
       <BookCreate onCreate={handleCreateBook} />
